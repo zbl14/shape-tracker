316b0a7ba2e2a625d69ab9be179bd0c72ab17cd9
zbl14 <ifthereisoneday@gmail.com>
2022-06-02 22:30:11 -0700

    BUGFIX: when 1st side length + 2nd side lenght = 3rd side lenght, it is not a triangle, previously it was scalene

--- js/scripts.js
+++ js/scripts.js
@@ -4,13 +4,15 @@ $(document).ready(function() {
     const number1 = parseInt($("#input1").val());
     const number2 = parseInt($("#input2").val());
     const number3 = parseInt($("#input3").val());
-    console.log(number1, number2, number3)
-    if (number1 === number2 && number1 === number3) {
-      $("#equilateral").show();
-    } else if (number1 === number2 && number1 !== number3 && number1 + number2 > number3) {
+    
+    if (number1 + number2 > number3 && number2 + number3 > number1 && number1 + number3 > number2) {
+      if (number1 === number2 && number1 === number3) {
+        $("#equilateral").show();
+      } else if (number1 === number2 && number1 !== number3) {
       $("#isosceles").show(); 
-    } else if (number1 !== number2 && number1 !==number3 && number2 !== number3 && number1 + number2 > number3 || number2 + number3 > number1 || number1 + number3 > number2) {
+      } else if (number1 !== number2 && number1 !==number3 && number2 !== number3) {
       $("#scalene").show();
+      } 
     } else {
       $("#notATriangle").show();
     }
