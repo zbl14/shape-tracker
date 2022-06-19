16dda1a45305acb0b411ceb91b1c1bc57a87f0ce
zbl14 <ifthereisoneday@gmail.com>
2022-06-19 00:34:18 -0700

    import Triangle constructor into UI logic

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+import { Triangle } from './triangle.js';
+
 $(document).ready(function() {
   $("form#triangle").submit(function(event) {
     event.preventDefault();
@@ -7,22 +9,5 @@ $(document).ready(function() {
     const triangle = new Triangle(length1, length2, length3)
     const response = triangle.checkType();
     $('#response').append("<p>" + response + "</p>");
-
-    
-    // if (number1 + number2 > number3 && number2 + number3 > number1 && number1 + number3 > number2) {
-    //   if (number1 === number2 && number1 === number3) {   
-    //     $("#response").show();   
-    //     $("#result").text("This is a equilateral.");
-    //   } else if (number1 === number2 && number1 !== number3) {
-    //     $("#conclusion").show();   
-    //     $("#result").text("This is a isosceles."); 
-    //   } else if (number1 !== number2 && number1 !==number3 && number2 !== number3) {
-    //     $("#conclusion").show();   
-    //     $("#result").text("This is a scalene");
-    //   } 
-    // } else {
-    //   $("#conclusion").show();   
-    //   $("#result").text("It is not a triangle.");
-    // }
   });
 });
--- src/triangle.js
+++ src/triangle.js
@@ -1,4 +1,4 @@
-function Triangle(side1, side2, side3) {
+export function Triangle(side1, side2, side3) {
   this.side1 = side1;
   this.side2 = side2;
   this.side3 = side3;
