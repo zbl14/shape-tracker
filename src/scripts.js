8855d2632ee74779a67646530b0f43338339781f
zbl14 <ifthereisoneday@gmail.com>
2022-06-19 00:06:12 -0700

    update ID of each length

--- src/scripts.js
+++ src/scripts.js
@@ -1,9 +1,9 @@
 $(document).ready(function() {
   $("form#triangle").submit(function(event) {
     event.preventDefault();
-    const number1 = parseInt($("#input1").val());
-    const number2 = parseInt($("#input2").val());
-    const number3 = parseInt($("#input3").val());
+    const number1 = parseInt($("#length1").val());
+    const number2 = parseInt($("#length2").val());
+    const number3 = parseInt($("#length3").val());
     
     if (number1 + number2 > number3 && number2 + number3 > number1 && number1 + number3 > number2) {
       if (number1 === number2 && number1 === number3) {   
