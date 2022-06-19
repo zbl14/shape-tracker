7372120f3974b37988e0ab86b0ae5daf2935aa06
zbl14 <ifthereisoneday@gmail.com>
2022-06-19 14:47:09 -0700

    brancing for identify the shape of a triangle

--- src/triangle.js
+++ src/triangle.js
@@ -5,20 +5,16 @@ export function Triangle(side1, side2, side3) {
 }
 
 Triangle.prototype.checkType = function() {
-  return "I can't answer that yet!";
-  // if (number1 + number2 > number3 && number2 + number3 > number1 && number1 + number3 > number2) {
-  //   if (number1 === number2 && number1 === number3) {   
-  //     $("#response").show();   
-  //     $("#result").text("This is a equilateral.");
-  //   } else if (number1 === number2 && number1 !== number3) {
-  //     $("#conclusion").show();   
-  //     $("#result").text("This is a isosceles."); 
-  //   } else if (number1 !== number2 && number1 !==number3 && number2 !== number3) {
-  //     $("#conclusion").show();   
-  //     $("#result").text("This is a scalene");
-  //   } 
-  // } else {
-  //   $("#conclusion").show();   
-  //   $("#result").text("It is not a triangle.");
-  // }
+  // return "I can't answer that yet!";
+  if (this.side1 + this.side2 > this.side3 && this.side2 + this.side3 > this.side1 && this.side1 + this.side3 > this.side2) {
+    if (this.side1 === this.side2 && this.side1 === this.side3) {   
+      return "This is a equilateral.";
+    } else if (this.side1 === this.side2 && this.side1 !== this.side3) {
+      return "This is a isosceles."; 
+    } else if (this.side1 !== this.side2 && this.side1 !==this.side3 && this.side2 !== this.side3) {
+      return "This is a scalene";
+    } 
+  } else {
+    return "It is not a triangle.";
+  }
 };
