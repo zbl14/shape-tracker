7369782fa88ea13945ab9502029c79427e96dbd8
zbl14 <ifthereisoneday@gmail.com>
2022-06-19 00:24:15 -0700

    WIP: Triangle.prototype.checkType

--- src/triangle.js
+++ src/triangle.js
@@ -6,4 +6,20 @@ function Triangle(side1, side2, side3) {
 
 Triangle.prototype.checkType = function() {
   return "I can't answer that yet!";
+
+    // if (number1 + number2 > number3 && number2 + number3 > number1 && number1 + number3 > number2) {
+    //   if (number1 === number2 && number1 === number3) {   
+    //     $("#response").show();   
+    //     $("#result").text("This is a equilateral.");
+    //   } else if (number1 === number2 && number1 !== number3) {
+    //     $("#conclusion").show();   
+    //     $("#result").text("This is a isosceles."); 
+    //   } else if (number1 !== number2 && number1 !==number3 && number2 !== number3) {
+    //     $("#conclusion").show();   
+    //     $("#result").text("This is a scalene");
+    //   } 
+    // } else {
+    //   $("#conclusion").show();   
+    //   $("#result").text("It is not a triangle.");
+    // }
 }
