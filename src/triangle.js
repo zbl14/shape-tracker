583b6e7047ee68a3a2cd4549ceea98407d0bb07b
zbl14 <ifthereisoneday@gmail.com>
2022-06-22 00:00:41 -0700

    update formate

--- src/triangle.js
+++ src/triangle.js
@@ -5,7 +5,6 @@ export function Triangle(side1, side2, side3) {
 }
 
 Triangle.prototype.checkType = function() {
-  // return "I can't answer that yet!";
   if (this.side1 + this.side2 > this.side3 && this.side2 + this.side3 > this.side1 && this.side1 + this.side3 > this.side2) {
     if (this.side1 === this.side2 && this.side1 === this.side3) {   
       return "This is a equilateral.";
